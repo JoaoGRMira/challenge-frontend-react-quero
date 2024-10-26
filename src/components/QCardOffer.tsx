3673c144c14c76cbb6a4a827a7df90b37464cad1
João Gabriel <joao.mira@fatec.sp.gov.br>
2024-10-26 17:01:12 -0300

    fix: card details
    Formatted card details.

--- src/components/QCardOffer.tsx
+++ src/components/QCardOffer.tsx
@@ -1,9 +1,9 @@
 import { FC } from "react";
-import QHeading from "./QHeading";
+import QText from "./QText";
 import QRating from "./QRating";
 import QPrice from "./QPrice";
-import QText from "./QText";
 import QButton from "./QButton";
+import QHeading from "./QHeading";
 
 interface QCardOfferProps {
     courseName: string;
@@ -17,19 +17,53 @@ interface QCardOfferProps {
     iesName: string;
 }
 
+// formatting course type
+const formatTipo = (tipo: string) => {
+    return tipo === "presencial" ? "Presencial" : tipo === "ead" ? "EaD" : tipo;
+};
+
+// formatting graduation degree
+const formatNivel = (nivel: string) => {
+    switch (nivel) {
+        case "bacharelado":
+            return "Graduação (bacharelado)";
+        case "tecnologo":
+            return "Graduação (tecnólogo)";
+        case "licenciatura":
+            return "Graduação (licenciatura)";
+        default:
+            return nivel;
+    }
+};
+
+// formatting price
+const formatarPreco = (preco: number) => {
+    return new Intl.NumberFormat("pt-BR", {
+        style: "currency", // style as currency
+        currency: "BRL", // currency in R$
+    }).format(preco);
+};
+
+// calculate discount percentage
+const calcularDesconto = (fullPrice: number, offeredPrice: number) => {
+    if (fullPrice && offeredPrice) {
+        const desconto = ((fullPrice - offeredPrice) / fullPrice) * 100; // calculate discount based on full price and offered price
+        return `${Math.round(desconto)}%`; // return discount percentage
+    }
+    return "0%";
+};
+
 const QCardOffer: FC<QCardOfferProps> = ({
     courseName,
     rating,
     fullPrice,
     offeredPrice,
-    discount,
     kind,
     level,
     iesLogo,
     iesName,
 }) => {
     return (
-        // min-h define minimum height for consistent card height
         <article className="bg-white p-6 rounded-lg shadow-sm border flex flex-col justify-between items-start gap-3 min-h-[355px]">
             <img src={iesLogo} alt={iesName} className="h-10 object-contain" />
             <QHeading tag="h2" size="sm">
@@ -37,14 +71,18 @@ const QCardOffer: FC<QCardOfferProps> = ({
             </QHeading>
             <QRating rating={rating} />
             <QPrice
-                fullPrice={fullPrice}
-                offeredPrice={offeredPrice}
-                discount={discount}
+                fullPrice={formatarPreco(parseFloat(fullPrice))} // fill with formatted full price
+                offeredPrice={formatarPreco(parseFloat(offeredPrice))} // fill with formatted offered price
+                discount={calcularDesconto(
+                    parseFloat(fullPrice),
+                    parseFloat(offeredPrice)
+                )} // fill with formatted discount percentage
             />
             <div>
-                <QText tag="p">{kind}</QText>
+                <QText tag="p">{formatTipo(kind)}</QText>
                 <QText tag="p" color="minor" size="sm">
-                    {level}
+                    {formatNivel(level)}{" "}
+                    {/* fill with formatted graduation degree */}
                 </QText>
             </div>
             <QButton tag="a" size="sm" className="w-full">
--- src/components/QRating.tsx
+++ src/components/QRating.tsx
@@ -7,15 +7,28 @@ interface QBadgeProps {
 }
 
 const QRating: FC<QBadgeProps> = ({ rating }) => {
+    const stars = Math.floor(rating); // number of full stars
+    const hasHalfStar = rating % 1 >= 0.5; // check if a half star should be displayed
+    const emptyStars = 5 - stars - (hasHalfStar ? 1 : 0); // number of empty stars
+
     return (
         <div className="flex items-center gap-2">
-            <QText tag="span">{rating}</QText>
+            <QText tag="span">{rating.toFixed(1)}</QText>
+            {/* displays the rating with one decimal place */}
             <div className="flex items-center space-x-1 text-yellow-500">
-                <QIconStar />
-                <QIconStar />
-                <QIconStar />
-                <QIconStar />
-                <QIconStar half />
+                {/* create a number of stars components equal to the number of full stars */}
+                {Array.from({ length: stars }, (_, index) => (
+                    <QIconStar key={index} />
+                ))}
+                {/* conditionally renders a half star component if applicable */}
+                {hasHalfStar && <QIconStar half />}
+                {/* create a number of stars components for empty stars based on the calculation */}
+                {Array.from({ length: emptyStars }, (_, index) => (
+                    <QIconStar
+                        key={index + stars + (hasHalfStar ? 1 : 0)}
+                        empty
+                    />
+                ))}
             </div>
         </div>
     );
